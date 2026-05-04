964584b20502354ebe7222aaff13b4eb54f33ae4
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-05 02:41:34 +0530

    updated program cs

---
+++ SupplySync.Infrastructure/Persistence/MongoDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using SupplySync.Infrastructure.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplySync.Infrastructure.Persistence
+{
+    class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IMongoClient client, IOptions<MongoDbSettings> settings)
+        {
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _database.GetCollection<T>(name);
+        }
+
+        public async Task PingAsync()
+        {
+            await _database.RunCommandAsync<BsonDocument>(
+                new BsonDocument { { "ping", 1 } }
+                );
+        }
+    }
+}
