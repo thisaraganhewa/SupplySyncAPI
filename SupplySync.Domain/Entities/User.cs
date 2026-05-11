fe778598d925f286e531f1372e2b7a32578394f8
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-11 06:44:53 +0530

    added user entity and added mongodb driver to domain

---
+++ SupplySync.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplySync.Domain.Entities
+{
+    class User
+    {
+        [BsonId]
+        public string Id { get; set; }
+    }
+}
