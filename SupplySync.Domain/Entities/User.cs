a1b1ceec6db1ba460be4e72cf751c3e7647050fd
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-13 20:00:18 +0530

    feat: added user entity in domain

--- SupplySync.Domain/Entities/User.cs
+++ SupplySync.Domain/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,10 @@ namespace SupplySync.Domain.Entities
     class User
     {
         [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
