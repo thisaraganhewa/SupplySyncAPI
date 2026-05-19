b5dc1091f0ba676010d68526219ef59b5cb7d215
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-19 21:32:30 +0530

    added user repo

---
+++ SupplySync.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using SupplySync.Application.Interfaces;
+using SupplySync.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplySync.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IMongoCollection<User> _userCollection;
+
+        public UserRepository(IMongoDatabase database)
+        {
+            _userCollection = database.GetCollection<User>("Users");
+        }
+
+        public Task<List<User>> GetAllUsersAsync()
+        {
+           
+        }
+    }
+}
