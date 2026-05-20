d74b5317252c9f68d495b45aa1cf70a9cdb2c38a
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-20 17:41:25 +0530

    feat: added UserService

---
+++ SupplySync.Application/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using SupplySync.Application.Interfaces;
+using SupplySync.Application.Interfaces.Service;
+using SupplySync.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplySync.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _userRepository.GetAllUsersAsync();
+        }
+    }
+}
