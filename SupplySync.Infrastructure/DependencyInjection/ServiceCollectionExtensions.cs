96fb40815d83149bb6347540531522c647895db2
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-05-26 07:55:51 +0530

    added dependacey injection .. need to impletement and add packages

---
+++ SupplySync.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplySync.Infrastructure.DependencyInjection
+{
+    public class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
