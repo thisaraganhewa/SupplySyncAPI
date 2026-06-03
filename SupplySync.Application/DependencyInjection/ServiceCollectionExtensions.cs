23c0aed1ccd095f98cd8f07d685b4bcf63c7bcd0
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-06-03 20:13:57 +0530

    feat: added dependencey injection for applications

--- SupplySync.Application/DependencyInjection/ServiceCollectionExtensions.cs
+++ SupplySync.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SupplySync.Application.DependencyInjection
 {
-    class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
     }
 }
