bd3923930fb0ca3c7b918af8a64fcd70f2a8d5ee
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-07-14 20:50:10 +0530

    feat: finished upda the infra setup

--- SupplySync.Application/obj/Debug/net9.0/SupplySync.Application.AssemblyInfo.cs
+++ SupplySync.Application/obj/Debug/net9.0/SupplySync.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupplySync.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3489e7136d566a0a71c79b2f0af5330f977351dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2824c341c26365765a468a4878666fbf99eb86f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupplySync.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupplySync.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
