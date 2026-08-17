2c536091a266850964b5a05b23b879299379e685
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2026-08-17 10:54:28 +0530

    feat : added RefreshToken modal

---
+++ SupplySync.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupplySync.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public string Id { get; private set; } = null!;
+
+        public string UserId { get; private set; } = null!;
+
+        public string TokenHash { get; private set; } = null!;
+
+        public DateTime ExpiresAt { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime? RevokedAt { get; private set; }
+
+        public bool IsRevoked => RevokedAt.HasValue;
+    }
+}
